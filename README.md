53f3b45d390950168f222fca31566600183b3882
root <jakkul@gmail.com>
2019-10-14 16:14:02 +0200

    help redone

--- README.md
+++ README.md
@@ -2,3 +2,37 @@
 nagios plugin for Temper.py
 
 https://pypi.org/project/temper-py/
+
+# help from the tool
+
+
+This bash script reads temperature from https://pypi.org/project/temper-py/ and returns in a format that is consumeable by Nagios. Also provides info for graphing.
+
+Input parameters:
+
+ -c critical level Celcius (default 30)
+ -w warning level Celcius (default 26)
+ -b BUS/device to use (default first one) (starts with 1)
+ -i which sensor on the USB are we using (default first one) (allowed values internal external)
+
+ be sure to set proper temper.py path and proper entitlements
+
+Output:
+
+ OK TEMP is 23.0 C on external at device 2 | temp=23.0;warn=30;crit=26
+
+ also provides return codes
+ 0 - OK
+ 1 - WARNING
+ 2 - CRITICAL
+ 3 - UNKNOWN
+
+Dependencies:
+
+ temper.py
+ bc
+ getopts
+
+Versions:
+
+ 0.1 runs, kinda, jakkul@gmail.com
--- check_temperpy.sh
+++ check_temperpy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-: <<'END_COMMENT'
+help () {
+
+cat <<'END_COMMENT'
 
 This bash script reads temperature from https://pypi.org/project/temper-py/ and returns in a format that is consumeable by Nagios. Also provides info for graphing.
 
@@ -8,14 +10,14 @@ Input parameters:
 
  -c critical level Celcius (default 30)
  -w warning level Celcius (default 26)
- -b BUS/device to use (default first one) (starts with 0)
+ -b BUS/device to use (default first one) (starts with 1)
  -i which sensor on the USB are we using (default first one) (allowed values internal external)
 
  be sure to set proper temper.py path and proper entitlements
 
 Output:
 
- Temperature OK - Sensor XXX ID YYY = 21.38C | temp=21.38;crit=30;warn=26;
+ OK TEMP is 23.0 C on external at device 2 | temp=23.0;warn=30;crit=26
 
  also provides return codes
  0 - OK
@@ -35,6 +37,8 @@ Versions:
 
 END_COMMENT
 
+}
+
 TEMPERPATH=temper.py
 
 DEFAULTCRIT=26
@@ -50,7 +54,7 @@ CRIT=$DEFAULTCRIT
 while getopts ":w:c:b:i:h" opt; do
   case ${opt} in
     h ) # help
-	    echo "HELP MESSAGE"
+	    help
 	    exit 0
       ;;
     w ) # set warning level
@@ -66,7 +70,7 @@ while getopts ":w:c:b:i:h" opt; do
 	SENSOR=$OPTARG
       ;;
 
-    \? ) echo "HELP MESSAGE"
+    \? ) echo "use $0 -h to get a help message"
 	    exit 3
       ;;
   esac
